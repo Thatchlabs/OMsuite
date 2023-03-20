707843122c681e938f2b4972724ef1f8b3dc0461
Slate452 <kenechukwuogbuagu@gmail.com>
2023-03-20 19:26:14 +0100

    UI Improvements

--- src/components/Containter/Home.tsx
+++ src/components/Containter/Home.tsx
@@ -7,13 +7,17 @@ import { HATSVG,AVATARSVG,GANGSVG,logoBlackSVG } from '../Elements/Images';
 
 const Home = () => {
     return (
-    <div className='container'>
-
-      <nav>
+    <div>
+      <main>
+      <h1>Welcome to the lab</h1>
+      <p>Get started ...</p>
+      </main>
+      <nav className='containerH'>
       <ul className='homeContainer'>
         <li className='homeItem'>{InfoCard("New Avatar", logoBlackSVG,AVATARSVG,'/OMsuite/#/avatars')}</li>
-        <li className='homeItem'>{InfoCard("New Gang", logoBlackSVG, GANGSVG,'/OMsuite/#/gangs')}</li>
+        <li className='homeItem'>{InfoCard("New Gang", logoBlackSVG,GANGSVG,'/OMsuite/#/gangs')}</li>
         <li className='homeItem'>{InfoCard("Get Items", logoBlackSVG,HATSVG,'/OMsuite/#/items')}</li>
+
       </ul>
       </nav>
     </div>
