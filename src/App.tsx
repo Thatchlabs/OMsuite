6c95b15d8c81e3b1dfd117593dd4ff8341b4611f
Slate452 <kenechukwuogbuagu@gmail.com>
2023-03-25 23:40:29 +0100

    Started Working ON Wallet Adapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import * as web3 from "@solana/web3.js";
 import ReactDOM from 'react-dom';
 import React, { FC, ReactElement, ReactNode, useMemo} from 'react';
 import './App.css';
@@ -7,18 +5,45 @@ import SideBar from './components/SideBar';
 import { Gangs } from "./components/Containter/Gangs";
 import { Personas } from "./components/Containter/Persona";
 import { Item } from "./components/Containter/getItem";
-import Home from "./components/Containter/Home";
+import {Home, HomeButton} from "./components/Containter/Home";
 import { Routes,Route } from "react-router-dom";
 import Coming from'./components/Containter/ComingSoon';
 import { Avatars } from "./components/Containter/getAvatar";
+
+import  '@solana/wallet-adapter-react-ui/styles.css'
+import * as web3 from "@solana/web3.js";
+import { ConnectionProvider, ConnectionProviderProps, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider,WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { GlowWalletAdapter,
+         PhantomWalletAdapter,
+         SlopeWalletAdapter,
+         SolflareWalletAdapter,
+         } from '@solana/wallet-adapter-wallets';  
 
 // APP
 function App() { 
   //const routeResults = useRoutes(routes)
   return (
     <div>
-                <HomeBu/>
-                <Routes>
+      <Context>
+      <Routes>
+        <Route index element ={<Home/>}/>
+        <Route path="/avatars" Component={Avatars}/>
+        <Route path="/gangs" Component={Gangs}/>
+        <Route path="/persona" Component={Personas}/>
+        <Route path="/items" Component={Item}/>
+        <Route path='/nftninja' element = {<Coming/>}/>
+      </Routes>
+      </Context>
+      <HomeButton/>
+      <SideBar/>
+      
+            </div>
+
+  );
+}
+
+/*<Routes>
                   <Route index element ={<Home/>}/>
                   <Route path="/avatars" Component={Avatars}/>
                   <Route path="/gangs" Component={Gangs}/>
@@ -26,35 +51,37 @@ function App() {
                   <Route path="/items" Component={Item}/>
                   <Route path='/nftninja' element = {<Coming/>}/>
                 </Routes>
-                <SideBar/>
-      </div>
-  );
-}
+ */
 
 export default App;
 
-function HomeBu():ReactElement {
 
-  return <div>
-  <div className='homeLogo'>
-              
-              <a href='/OMsuite/#/' className='nav-link'>
-              <svg id="Layer_1" 
-                   data-name="Layer 1" 
-                   xmlns="http://www.w3.org/2000/svg" 
-                   viewBox="0 0 381.38 324.09">
-                  <path className="logo-color-1"
-                   fill ="currentColor"
-                   d="M72.25,410l55.14-260.33H64.05L77.73,85.91H277.86l-13.47,63.76H204L148.86,410Z" 
-                   transform="translate(-64.05 -85.91)"/>
-                  <path className="logo-color-2"
-                  fill ="currentColor"
-                    d="M255,410,324,85.91h76.19L345,346.23H445.42L432.16,410Z" 
-                   transform="translate(-64.05 -85.91)"/>
-                   </svg>
+const Context:FC <{children:ReactNode}> = ({children}) =>{
+    //const network
+    //const endpoint 
+     const endpoint = 'https://solana-devnet.g.alchemy.com/v2/veVJEevaVLd2oLqL6aAE3nSCCPPzBixg';
+     const wallets= useMemo( ()=> [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),         
+    ],[]);
 
-                <span className="link-text">Thatch Labs</span>
-              </a>
-        </div>
-        </div>
+    return(
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets = {wallets} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    )
+
+}
+
+
+const Content:FC = ()=>{
+  return(
+    <div className='main'>
+      <WalletMultiButton/>
+    </div>
+  )
 }
--- src/components/Containter/Home.tsx
+++ src/components/Containter/Home.tsx
@@ -2,9 +2,19 @@ import React, { ReactElement, ReactNode } from 'react';
 import { InfoCard } from '../Elements/Cards';
 import { HATSVG,AVATARSVG,GANGSVG,logoBlackSVG } from '../Elements/Images';
 
+import  '@solana/wallet-adapter-react-ui/styles.css'
+import * as web3 from "@solana/web3.js";
+import { ConnectionProvider, ConnectionProviderProps, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider,WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { GlowWalletAdapter,
+         PhantomWalletAdapter,
+         SlopeWalletAdapter,
+         SolflareWalletAdapter,
+         } from '@solana/wallet-adapter-wallets';  
 
 
-const Home = () => {
+
+export const Home = () => {
   
   
     return (
@@ -12,6 +22,9 @@ const Home = () => {
       <main>
         <h1>Welcome to the lab</h1>
         <p>Get started 😉...</p>
+        <div className='login'>
+            <WalletMultiButton/>
+        </div>
         
       </main>
       <nav className='containerH'>
@@ -25,4 +38,29 @@ const Home = () => {
     );
   };
   
-  export default Home;
+
+export function HomeButton():ReactElement {
+
+    return <div>
+    <div className='homeLogo'>
+                
+                <a href='/OMsuite/#/' className='nav-link'>
+                <svg id="Layer_1" 
+                     data-name="Layer 1" 
+                     xmlns="http://www.w3.org/2000/svg" 
+                     viewBox="0 0 381.38 324.09">
+                    <path className="logo-color-1"
+                     fill ="currentColor"
+                     d="M72.25,410l55.14-260.33H64.05L77.73,85.91H277.86l-13.47,63.76H204L148.86,410Z" 
+                     transform="translate(-64.05 -85.91)"/>
+                    <path className="logo-color-2"
+                    fill ="currentColor"
+                      d="M255,410,324,85.91h76.19L345,346.23H445.42L432.16,410Z" 
+                     transform="translate(-64.05 -85.91)"/>
+                     </svg>
+  
+                  <span className="link-text">Thatch Labs</span>
+                </a>
+          </div>
+          </div>
+  }
